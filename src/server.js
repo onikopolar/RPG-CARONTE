d0bfb640cf663ccb72f6338659e8dab5ff8018d1
onikopolar <sangust1pc2@gmail.com>
2025-10-22 05:45:30 -0400

    fix: server.js corrigido e build funcionando

--- src/server.js
+++ src/server.js
@@ -1,39 +1,27 @@
-require('dotenv').config();
+const { createServer } = require('http')
+const { parse } = require('url')
+const next = require('next')
 
-const app = require('express')();
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
-const next = require('next');
+const dev = process.env.NODE_ENV !== 'production'
+const hostname = '0.0.0.0'
+const port = process.env.PORT || 3000
 
-const dev = process.env.NODE_ENV !== 'production';
+// Quando rodando no Heroku, não use hostname específico
+const app = next({ dev })
+const handle = app.getRequestHandler()
 
-const nextApp = next({ dev });
-const nextHandler = nextApp.getRequestHandler();
-
-io.on('connect', socket => {
-    socket.on('room:join', roomName => {
-        return socket.join(roomName);
-    });
-
-    socket.on('update_hit_points', data => {
-        return io.to(`portrait_character_${data.character_id}`).emit('update_hit_points', data);
-    });
-
-    socket.on('dice_roll', data => {
-        return io.to(`dice_character_${data.character_id}`).emit('dice_roll', data);
-    });
-});
-
-nextApp.prepare().then(() => {
-    app.all('*', (req, res) => {
-        return nextHandler(req, res);
-    });
-
-    server.listen(process.env.PORT || 3000, err => {
-        if(err) {
-            throw err;
-        }
-
-        console.log('[Server] Successfully started on port', process.env.PORT || 3000);
-    });
+app.prepare().then(() => {
+  createServer(async (req, res) => {
+    try {
+      const parsedUrl = parse(req.url, true)
+      await handle(req, res, parsedUrl)
+    } catch (err) {
+      console.error('Error occurred handling', req.url, err)
+      res.statusCode = 500
+      res.end('internal server error')
+    }
+  }).listen(port, (err) => {
+    if (err) throw err
+    console.log(`> Ready on http://localhost:${port}`)
+  })
 })
